826e0199bee483c2ffc8be773f434f64067d4968
knb <rma6VBRe!00>
2021-12-14 10:29:03 +0100

    Fix für GUI Designer Fix für Sound

--- src/main/java/my_project/control/ProgramController.java
+++ src/main/java/my_project/control/ProgramController.java
@@ -1,9 +1,8 @@
 package my_project.control;
 
 import KAGO_framework.control.ViewController;
-import KAGO_framework.model.abitur.datenstrukturen.Queue;
-import my_project.model.Ball;
-import my_project.view.InputManager;
+import my_project.model.KnebiParser;
+import my_project.view.MainGUI;
 
 import java.awt.event.MouseEvent;
 
@@ -18,6 +17,7 @@ public class ProgramController {
 
     // Referenzen
     private ViewController viewController;  // diese Referenz soll auf ein Objekt der Klasse viewController zeigen. Über dieses Objekt wird das Fenster gesteuert.
+    private KnebiParser knebiParser;
 
     /**
      * Konstruktor
@@ -31,21 +31,84 @@ public class ProgramController {
     }
 
     /**
-     * Diese Methode wird genau ein mal nach Programmstart aufgerufen.
-     * Sie erstellt die leeren Datenstrukturen, zu Beginn nur eine Queue
+     * Diese Methode wird genau ein mal nach Programmstart aufgerufen. Achtung: funktioniert nicht im Szenario-Modus
      */
     public void startProgram() {
-        // Erstelle ein Objekt der Klasse Ball und lasse es zeichnen
-        Ball ball1 = new Ball(150,150);
-        viewController.draw(ball1);
+        viewController.getDrawFrame().setSize(600,320);
+        viewController.getDrawFrame().setContentPane(new MainGUI(this).getMainPanel());
+        viewController.getSoundController().loadSound("src/main/resources/sound/yesyesyes.mp3","yes",false);
+        viewController.getSoundController().loadSound("src/main/resources/sound/nonono.mp3","no",false);
+        knebiParser = new KnebiParser();
+        // todo Eigener Code
 
     }
 
     /**
-     * Aufruf mit jeder Frame
+     * Diese Methode parst den übergebenen String mit einem Parser einer Sprache.
+     * @param input der zu parsende String
+     * @param parserIndex der Index des zu verwendenden Parsers (0 = Knebiparser, Rest ist frei)
+     * @return true, fallse der String ein Wort der Sprache des Parsers ist.
+     */
+    public boolean parseString(String input, int parserIndex){
+        switch(parserIndex){
+            case 0:
+                return knebiParser.parse(input);
+            // todo Hier können weitere Parser aufgeführt werden
+            case 1:
+                return knebiParser.parse(input);
+
+            default: System.out.println("\nDebug-Info: Für diesen Index ist kein Parser definiert!");
+        }
+        return false;
+    }
+
+    /**
+     * Der übergebene String wird gescannt.
+     * @param input der zu scannende String
+     * @param parserIndex der zu verwendende Scanner (ist dem Parser zugeordnet, daher Parserindex)
+     * @return true, falls der Scan erfolgreich war, andernfalls false
+     */
+    public boolean scanString(String input, int parserIndex){
+        switch(parserIndex){
+            case 0:
+                boolean result = knebiParser.getScannerResult(input);
+                System.out.println("\n- KnebiScanner-DEBUG - "+knebiParser.getScannerOutput());
+                return result;
+            // todo Hier können weitere Scanner aufgeführt werden.
+            case 1:
+                return knebiParser.getScannerResult(input);
+
+            default: System.out.println("\nDebug-Info: Für diesen Index ist kein Scanner definiert!");
+        }
+        return false;
+    }
+
+    // ********* FRAMEWORK-METHODEN *************
+
+    /**
+     * Sorgt dafür, dass zunächst gewartet wird, damit der SoundController die
+     * Initialisierung abschließen kann. Die Wartezeit ist fest und damit nicht ganz sauber
+     * implementiert, aber dafür funktioniert das Programm auch bei falscher Java-Version
      * @param dt Zeit seit letzter Frame
      */
     public void updateProgram(double dt){
 
+    }
+
+
+    /**
+     * Verarbeitet einen Mausklick.
+     * @param e das Objekt enthält alle Informationen zum Klick
+     */
+    public void mouseClicked(MouseEvent e){
+
+    }
+
+    public void playYes(){
+        viewController.getSoundController().playSound("yes");
+    }
+
+    public void playNo(){
+        viewController.getSoundController().playSound("no");
     }
 }
